be5a4a373b57cff5c0df12e6ee78cd0cc1b11aed
Marios Kanellopoulos <mariosknl@outlook.com>
2023-05-21 10:31:39 +0300

    update user profile endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,23 @@ import User from "../models/userModel.js";
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
-const authUser = asyncHandler(async (req, res) => {});
+const authUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+
+	if (user && (await user.matchPassword(password))) {
+		generateToken(res, user._id);
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+		});
+	} else {
+		res.status(401);
+		throw new Error("Invalid email or password");
+	}
+});
 
 // @desc Register new user
 // route POST /api/users
@@ -43,21 +59,53 @@ const registerUser = asyncHandler(async (req, res) => {
 // route POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Logout User" });
+	res.cookie("jwt", "", {
+		httpOnly: true,
+		expires: new Date(0),
+		secure: process.env.NODE_ENV !== "development",
+		sameSite: "strict",
+	});
+
+	res.status(200).json({ message: "User Logout successful" });
 });
 
 // @desc Get user profile
 // route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "User Profile" });
+	const user = {
+		_id: req.user._id,
+		name: req.user.name,
+		email: req.user.email,
+	};
+	res.status(200).json(user);
 });
 
 // @desc Update user profile
 // route PUT /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Update user profile" });
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+
+		if (req.body.password) {
+			user.password = req.body.password;
+		}
+
+		const updatedUser = await user.save();
+
+		res.status(200).json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+		});
+	} else {
+		res.status(404);
+		throw new Error("User not found");
+	}
 });
 
 export {
